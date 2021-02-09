5eb2c79aeb1b145cb3ae05564a141a6189adce9d
Guiselly Aroche <guisellyaroche@outlook.com>
2021-02-09 03:35:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    return "fizz"
+    return "Fizz"
 
   elsif int % 5 == 0
-    return "buzz"
+    return "Buzz"
 
   elsif int % 3 == 0 && int % 5 == 0
     return "fizzbuzz"
